86eaec9dd538bbe596a886696f4256e34e219a84
deeksha <deekshaaakk98@gmail.com>
2018-09-01 23:19:59 +0530

    changes in blogpost

--- pages/views.py
+++ pages/views.py
@@ -3,17 +3,16 @@ from pages.models import Posts
 from pages.models import BlogPost
 
 def post_list(request):
-	posts=Posts.objects.all()
+	post=Posts.objects.all()
 	# myposts = Posts.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request,"blog/post_list.html",{"post":posts})
-
+	return render(request,"blog/post_list.html",{"post":post})
 
 def home(request):
 	posts=BlogPost.objects.all()
 	return render(request,"home.html",{"posts":posts})
 
 def post_page(request,post_id):
-	post=BlogPost.objects.get(pk=post_id)
-	context= {"post":post }
+	post=Posts.objects.get(pk=post_id)
+	context= {"post":post}
 	return render(request,"post.html",context)
 
